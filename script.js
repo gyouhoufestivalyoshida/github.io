6780768626a4db0a2d2adc875fbec3de33362829
gyouhoufestival_nagano <kirakirasub0328@gmail.com>
2026-05-13 21:25:22 +0900

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAAlxZGPH72mvgePyM3RX_xy8xlhfv92Rk",
-  authDomain: "yoshidagyouhoufestival1-5.firebaseapp.com",
-  databaseURL: "https://yoshidagyouhoufestival1-5-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "yoshidagyouhoufestival1-5"
+    authDomain: "yoshidagyouhoufestival1-5.firebaseapp.com",
+    databaseURL: "https://yoshidagyouhoufestival1-5-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "yoshidagyouhoufestival1-5",
+    storageBucket: "yoshidagyouhoufestival1-5.firebasestorage.app",
+    messagingSenderId: "439419753147",
+    appId: "1:439419753147:web:f1b5a52e69d4e6e42f0f9e"
 };
 
 const app = initializeApp(firebaseConfig);
